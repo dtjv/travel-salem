179b9bd1781fc641f27433b67210f74120586360
David Valles <davidtjvalles@gmail.com>
2021-01-14 22:39:52 -0800

    Add search field

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -21,13 +21,21 @@ export const Nav = () => (
           </Link>
         </div>
         <div className="flex items-center space-x-4">
-          <div>
-            <Link href="/about">
-              <a className="font-medium text-gray-500 hover:text-blue-600">
-                About
-              </a>
-            </Link>
-          </div>
+          <input
+            type="text"
+            className="hidden bg-gray-100 border-gray-300 sm:block rounded-md focus:border-black focus:ring-0"
+            placeholder="Search"
+          />
+          <Link href="/">
+            <a className="font-medium text-gray-500 hover:text-blue-600">
+              Blog
+            </a>
+          </Link>
+          <Link href="/">
+            <a className="font-medium text-gray-500 hover:text-blue-600">
+              About
+            </a>
+          </Link>
         </div>
       </ContainerX>
     </div>
