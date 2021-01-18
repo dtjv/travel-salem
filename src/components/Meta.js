57365927f8c8a2b81f8fc4c5e65397e15fa3f0d5
David Valles <davidtjvalles@gmail.com>
2021-01-18 13:44:25 -0800

    Add meta data

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -1,1 +1,30 @@
-export const Meta = () => <></>
+import Head from 'next/head'
+import { site } from '../data/site'
+
+export const Meta = (props) => {
+  const title = props.title ?? site.title
+  const url = props.url ?? site.url
+  const description = props.description ?? site.description
+
+  return (
+    <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+      <meta name="lang" content="en" />
+      <meta name="theme-color" content="#000" />
+      <meta name="description" content={description} />
+
+      <meta name="og:url" content={url} />
+      <meta name="og:type" content="website" />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+
+      <title>{title}</title>
+    </Head>
+  )
+}
---
+++ src/data/site.js
@@ -0,0 +1,5 @@
+export const site = {
+  title: 'Travel Salem (demo)',
+  url: 'https://travelsalem.vercel.app',
+  description: 'A concept UI for Travel Salem',
+}
