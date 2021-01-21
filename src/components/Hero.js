850c2c6c537651caf7a949fbc51c10eac4f2059c
David Valles <davidtjvalles@gmail.com>
2021-01-21 15:00:19 -0800

    Adjust hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ import { ContainerX } from './Container'
 export const Hero = () => {
   return (
     <div className="relative">
-      <div className="relative w-full h-96">
+      <div className="relative w-full h-52 md:h-96">
         <Image
           src="/images/vineyard.jpg"
           layout="fill"
@@ -14,7 +14,7 @@ export const Hero = () => {
         />
       </div>
       <ContainerX className="absolute top-0">
-        <h1 className="text-3xl font-extrabold text-white sm:text-5xl md:text-6xl pt-52">
+        <h1 className="pt-24 text-3xl font-extrabold text-white sm:text-5xl md:text-6xl md:pt-52">
           Welcome to Salem.
         </h1>
         <p className="pt-1 italic font-bold tracking-wide text-white text-md sm:text-2xl md:text-3xl">
