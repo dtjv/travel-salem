f9e8ddddf2423a8219d37ab45ea53290b4cc4716
David Valles <davidtjvalles@gmail.com>
2021-01-22 07:56:06 -0800

    Redo horiz scoll (again)

--- src/components/Food.js
+++ src/components/Food.js
@@ -6,33 +6,28 @@ import { ContainerX, ContainerY } from './Container'
 
 const Item = ({ item }) => {
   return (
-    <li className="flex-grow-0 flex-shrink-0 first:-mr-4 sm:first:mr-4 lg:first:mr-10 xl:first:mr-24 first:w-0 last:w-0 w-36 last:pr-6 sm:last:pr-10 md:last:pr-14 lg:last:pr-24 xl:last:pr-36">
-      {!item.name ? null : (
-        <Link href="/">
-          <a aria-label={`link to ${item.name}`}>
-            <Image
-              className="rounded-lg"
-              quality={100}
-              src={`/images/${slugify(item.name)}.jpg`}
-              alt={`image for ${item.name}`}
-              layout="responsive"
-              width={500}
-              height={500}
-            />
-            <h3 className="mt-2 text-xl font-semibold text-gray-800">
-              {item.name}
-            </h3>
-          </a>
-        </Link>
-      )}
+    <li className="inline-block w-40 mx-2 md:w-48 lg:mx-4 lg:w-64 first:ml-4 sm:first:ml-8 md:first:ml-12 xl:first:ml-32 last:mr-16 md:last:mr-20 lg:first:ml-20 lg:last:mr-24 xl:last:mr-32">
+      <Link href="/">
+        <a aria-label={`link to ${item.name}`}>
+          <Image
+            className="rounded-lg"
+            quality={100}
+            src={`/images/${slugify(item.name)}.jpg`}
+            alt={`image for ${item.name}`}
+            layout="responsive"
+            width={500}
+            height={500}
+          />
+          <h3 className="mt-2 text-xl font-semibold text-gray-800">
+            {item.name}
+          </h3>
+        </a>
+      </Link>
     </li>
   )
 }
 
 export const Food = ({ section }) => {
-  section.links.unshift({})
-  section.links.push({})
-
   const links = section.links.map((link, idx) => <Item key={idx} item={link} />)
 
   return (
@@ -48,7 +43,7 @@ export const Food = ({ section }) => {
         </div>
       </ContainerX>
       <div>
-        <ul className="flex justify-between overflow-x-auto space-x-8 lg:space-x-10 hide-scrollbar flex-nowrap">
+        <ul className="overflow-x-auto hide-scrollbar whitespace-nowrap">
           {links}
         </ul>
       </div>
